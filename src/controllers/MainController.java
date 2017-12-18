88149e3623393232775021641a217755ddf2e2b0
SynysterLove <synlgp@gmail.com>
2017-12-17 22:44:23 -0300

    Pequeña correccion de como se instancia la calculadora

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -8,10 +8,17 @@ import model.CalculadoraGases;
 
 public class MainController {
 
+    private CalculadoraGases calc;
     @FXML private Label tempResultText;
     @FXML private TextField presionIn;
     @FXML private TextField volumenIn;
     @FXML private TextField molesIn;
+
+    public MainController() {
+
+        this.calc = new CalculadoraGases();
+
+    }
 
     @FXML
     private void handleButtonCalcular(ActionEvent event) {
@@ -19,9 +26,7 @@ public class MainController {
         Double presionDouble = Double.parseDouble(presionIn.getText());
         Double volumenDouble = Double.parseDouble(volumenIn.getText());
         Integer molesInt = Integer.parseInt(molesIn.getText());
-
-        CalculadoraGases calc = new CalculadoraGases();
-        Double temp = calc.calcularTemperatura(presionDouble, volumenDouble, molesInt);
+        Double temp = this.calc.calcularTemperatura(presionDouble, volumenDouble, molesInt);
 
         tempResultText.setText(temp.toString());
     }
